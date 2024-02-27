15e913423bb26e7c74da67872a1deae5e2663810
dzdidi <deniszalessky@gmail.com>
2024-02-27 17:29:51 +0000

    extract method and path
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- auth/index.js
+++ auth/index.js
@@ -9,10 +9,26 @@ const server = http.createServer((req, res) => {
     const event = JSON.parse(Buffer.from(authHeader, 'base64').toString())
   }
 
-  // TODO: validate event
-  
-  const { method, url } = req;
-  console.log(`Received ${method} request on ${url}`);
+  const url = req.headers['x-original-uri']
+  const method = req.headers['x-original-method']
+  if (!url || !method) {
+    res.writeHead(403);
+    return res.end();
+  }
+
+  const repoName = url.split('/').filter(Boolean)[0];
+  const action = url.split('/').filter(Boolean).pop();
+  if (!repoName || !action) {
+    res.writeHead(403);
+    return res.end();
+  }
+
+  if (method !== 'POST' || action !== 'git-receive-pack') {
+    res.writeHead(200);
+    return res.end();
+  }
+
+  console.log(`Will do auth in ${repoName} for ${action}`);
 
   res.writeHead(200);
   res.end();
